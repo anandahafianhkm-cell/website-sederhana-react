2214198da6fda1fa2e9ebadbe0ba7cd9f38b1c47
Ananda Hafian <anandahafianhkm@gmail.comm>
2026-03-25 16:17:15 +0700

    proses menyelesaikan homejsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { homeSection } from '../Data/HomeSection'
+import '../styles/Home.css'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <div className='wrapper'>
-      <section id="home">
+      <section className="home">
         <img src={homeSection.image}/>
         <div className='kolom'>
           <p className='deskripsi'>{homeSection.heading}</p>
+          <h1 className='deskripsi2'>{homeSection.subheading}</h1>
+          <p className='deskripsi3'>{homeSection.description}</p>
+          <button className='button'>{homeSection.linktombol}</button>
         </div>
       </section>
     </div>

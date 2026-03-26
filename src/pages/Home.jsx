7dc100e251fd9afb78bb48e98cc6bd483713ac83
Ananda Hafian <anandahafianhkm@gmail.comm>
2026-03-26 19:48:12 +0700

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,24 +7,36 @@ import '../styles/Home.css'
 function Home() {
   return (
     <>
-    <Navbar/>
-    <div className='wrapper'>
-      <section className="home">
-        <img src={homeSection.image}/>
-        <div className='kolom'>
-          <p className='deskripsi'>{homeSection.heading}</p>
-          <h1 className='deskripsi2'>{homeSection.subheading}</h1>
-          <p className='deskripsi3'>{homeSection.description}</p>
-          <button className='button'>{homeSection.linktombol}</button>
-        </div>
-      </section>
+      <Navbar />
+      <div className='wrapper'>
+        <section className="home">
+          <img src={homeSection.image} />
+          <div className='kolom'>
+            <p className='deskripsi'>{homeSection.heading}</p>
+            <h1 className='deskripsi2'>{homeSection.subheading}</h1>
+            <p className='deskripsi3'>{homeSection.description}</p>
+            <button className='button'>{homeSection.linktombol}</button>
+          </div>
+        </section>
 
-      {/* <section className="course">
-        <img src={courseSection.image}/>
-      </section> */}
-    </div>
 
-    <Footer/>
+      </div>
+
+      <div className='wrapper2'>
+        <section className="course">
+          <img src={courseSection.image} />
+          <div>
+            <p>{courseSection.paragraf}</p>
+            <p>{courseSection.heading}</p>
+            <h1>{courseSection.subheading}</h1>
+            <p>{courseSection.description}</p>
+            <button>{courseSection.linktombol}</button>
+          </div>
+        </section>
+      </div>
+
+
+      <Footer />
     </>
   )
 }
